c84f1976b29804e7eddd33fbccb07648a7015137
Keuntae Kim <keuntaekim1994@gmail.com>
2023-10-08 16:07:28 -0400

    First update: Added the 'SnakeGame' Folder, 'snakegame.jl' in it, and 'runsnake.jl'.

---
+++ SnakeGame/snakegame.jl
@@ -0,0 +1,12 @@
+# Screen Resolution
+WIDTH = 800
+HEIGHT = 600
+BACKGROUND = colorant"white"
+
+side_info_bar = 200
+
+# Snake(Actor)
+snake_x = (WIDTH - side_info_bar) / 2 + side_info_bar
+snake_y = HEIGHT / 2
+snake_size = 30
+snake_color = colorant"blue"
