577cf8fc9eda3dc66025386ec16a26cb3438d2a5
Harrison832-beep <wharrison832@gmail.com>
2023-10-13 00:30:07 -0400

    Added game asset folders

--- SnakeGame/snakegame.jl
+++ SnakeGame/snakegame.jl
@@ -10,3 +10,15 @@ snake_x = (WIDTH - side_info_bar) / 2 + side_info_bar
 snake_y = HEIGHT / 2
 snake_size = 30
 snake_color = colorant"blue"
+
+text_actor = TextActor("Snake Game", "snakegame", font_size=72, color=[0, 0, 0, 0])
+text_actor.pos = (100, 200)
+
+function update()
+    nothing
+end
+
+function draw()
+    draw(text_actor)
+end
+
