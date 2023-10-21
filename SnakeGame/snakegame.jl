a77f297cc07ff2636eea51644c215e85cc615213
Keuntae Kim <keuntaekim1994@gmail.com>
2023-10-21 19:53:57 -0400

    10/21/2023 - Changed background color and updated the variable name starting with 'snakehead' to 'snake_head'

--- SnakeGame/snakegame.jl
+++ SnakeGame/snakegame.jl
@@ -1,22 +1,25 @@
-using GameZero, DataStructures
-# Screen Resolution
+using DataStructures
+
+###########
+# Display #
+###########
 WIDTH = 800
 HEIGHT = 600
-BACKGROUND = colorant"white"
-
 side_info_bar = 200
+BACKGROUND = colorant"#B2E684"
 
-# Snake(Actor)
+#####################################
+# Default info for the Snake(Actor) #
+#####################################
 snake_x = (WIDTH - side_info_bar) / 2 + side_info_bar
 snake_y = HEIGHT / 2
 snake_size = 10
 snake_color = colorant"blue"
 
-snakehead = Rect(0, 
-                0, 
-                snake_size, 
-                snake_size)
-snakehead_lastpos = (0, 0)
+snake_head = Rect(
+    0, 0, snake_size, snake_size
+)
+snake_head_lastpos = (0, 0)
 snakebody = Queue{Rect}()  # Snake vector for storing snake body, first item is snake head
 obstacles = []
 apple = Rect(0, 0, snake_size, snake_size)
@@ -25,43 +28,43 @@ dx = 0
 dy = 10
 
 
-function update_snake_length!(snakebody::Queue{Rect}, snakehead_lastpos, snake_size)
-    enqueue!(snakebody, Rect(snakehead_lastpos, (snake_size, snake_size)))
+function update_snake_length!(snakebody::Queue{Rect}, snake_head_lastpos, snake_size)
+    enqueue!(snakebody, Rect(snake_head_lastpos, (snake_size, snake_size)))
 end
 
 
-function update_snake_pos!(snakehead, snakehead_lastpos, snakebody::Queue{Rect}, dx, dy)
+function update_snake_pos!(snake_head, snake_head_lastpos, snakebody::Queue{Rect}, dx, dy)
     """
     Update snake position, also check if snake has 
     * reached beyond border, if go ouside border, snake head appears on the other side of the map
     * collided with apple, increase snake length and regenerate apple position
     * collided with obstacles, game over
     """
-    snakehead_lastpos = (snakehead.x, snakehead.y)
-    snakehead.x += dx
-    snakehead.y += dy
-    update_snake_length!(snakebody, snakehead_lastpos, snake_size)
+    snake_head_lastpos = (snake_head.x, snake_head.y)
+    snake_head.x += dx
+    snake_head.y += dy
+    update_snake_length!(snakebody, snake_head_lastpos, snake_size)
     dequeue!(snakebody)
 
     # Check if reach border
     #  if border is reached and no obstacles, 
     #  snake head should appear on another end
-    if snakehead.x == WIDTH
-        snakehead.x = 0
+    if snake_head.x == WIDTH
+        snake_head.x = 0
     end
-    if snakehead.x < 0
-        snakehead.x = WIDTH - snake_size
+    if snake_head.x < 0
+        snake_head.x = WIDTH - snake_size
     end
-    if snakehead.y == HEIGHT
-        snakehead.y = 0
+    if snake_head.y == HEIGHT
+        snake_head.y = 0
     end
 
-    if snakehead.y < 0
-        snakehead.y = HEIGHT - snake_size
+    if snake_head.y < 0
+        snake_head.y = HEIGHT - snake_size
     end
 
     # Check collision
-    if collide(snakehead, apple)
+    if collide(snake_head, apple)
         play_sound("ea")
         # y, fs = wavread("$(@__DIR__)/sounds/ea.wav")
         # Threads.@spawn wavplay(y, fs)
@@ -74,7 +77,7 @@ function draw()
     Game loop
     """
     # Draw snake
-    draw(snakehead, colorant"gray", fill=true)
+    draw(snake_head, colorant"gray", fill=true)
     for s in snakebody
         draw(s, colorant"black", fill=true)
     end
@@ -93,7 +96,7 @@ function update()
     """
     Game loop
     """
-    update_snake_pos!(snakehead, snakehead_lastpos, snakebody, dx, dy)
+    update_snake_pos!(snake_head, snake_head_lastpos, snakebody, dx, dy)
     sleep(0.05)
 end
 
@@ -131,7 +134,9 @@ function on_key_down(g::Game, key)
 
 end
 
-
+#######
+# Map #
+#######
 function build_map()
     """
     Build map from text files in ./maps/:
@@ -140,7 +145,7 @@ function build_map()
     '\$' represents Actor initial position
     '@' represents Apple initial position
     """
-    global WIDTH, HEIGHT, obstacles, snakehead, snakehead_lastpos, apple
+    global WIDTH, HEIGHT, obstacles, snake_head, snake_head_lastpos, apple
     file = open("$(@__DIR__)/maps/level1.txt")
     
     h = 0
@@ -160,9 +165,9 @@ function build_map()
         if '\$' in line
             for (i, c) in enumerate(line)
                 if c == '\$'  # Actor found
-                    snakehead.x = (i-1)*10
-                    snakehead.y = (h-1)*10
-                    snakehead_lastpos = (snakehead.x, snakehead.y)
+                    snake_head.x = (i-1)*10
+                    snake_head.y = (h-1)*10
+                    snake_head_lastpos = (snake_head.x, snake_head.y)
                 end
             end
         end
@@ -181,10 +186,13 @@ function build_map()
 end
 
 
+#################
+# Main function #
+#################
 function main()
     build_map()
-    update_snake_pos!(snakehead, snakehead_lastpos, snakebody, dx, dy)
-    update_snake_length!(snakebody, snakehead_lastpos, snake_size)
+    update_snake_pos!(snake_head, snake_head_lastpos, snakebody, dx, dy)
+    update_snake_length!(snakebody, snake_head_lastpos, snake_size)
 end
 
 main()
